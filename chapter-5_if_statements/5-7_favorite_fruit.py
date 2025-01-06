3431e2e6bc4b2fc7e93885275ab39b88695d2b80
Angie <i4ngela@gmail.com>
2025-01-05 21:09:36 -0300

    Exercose 5.7 solved.

---
+++ chapter-5_if_statements/5-7_favorite_fruit.py
@@ -0,0 +1,19 @@
+"""Make a list of your favorite fruits, and then write a series
+of independent if statements that check for certain fruits in your list.
+• Make a list of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of
+fruit is in your list. If the fruit is in your list, the if block should
+print a statement, such as You really like bananas!"""
+
+favorite_fruits = ["mango", "grape", "banana"]
+
+if "mango" in favorite_fruits:
+    print("Ypu really like mango!")
+if "grape" in favorite_fruits:
+    print("You realy like grapes!")
+if "banana" in favorite_fruits:
+    print("You really like banana!")
+if "watermelon" in favorite_fruits:
+    print("You really like watermelon!")
+if "apple" in favorite_fruits:
+    print("You really like apple!")
