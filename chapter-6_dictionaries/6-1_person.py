ca1554e771b0cc0e8b7c15c5f45044c391a5a7ad
Angie <i4ngela@gmail.com>
2025-01-07 15:00:44 -0300

    Exercise 6.1 solved.

---
+++ chapter-6_dictionaries/6-1_person.py
@@ -0,0 +1,11 @@
+"""Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live.
+You should have keys such as first_name, last_name, age, and city.
+Print each piece of information stored in your dictionary."""
+
+person = {"first_name": "angela", "last_name": "lopes", "age": 27, "city": "marília"}
+
+print(f"First name: {person['first_name'].title()}")
+print(f"Last name: {person['last_name'].title()}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city'].title()}")
