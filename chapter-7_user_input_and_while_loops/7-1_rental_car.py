0ccb5d9ad3369af7ec85c1eb6f5f5952133862a0
Angie <i4ngela@gmail.com>
2025-01-12 17:06:05 -0300

    Exercise 7.1 solved.

---
+++ chapter-7_user_input_and_while_loops/7-1_rental_car.py
@@ -0,0 +1,5 @@
+"""Write a program that asks the user what kind of rental car they would like.
+Print a message about that car, such as 'Let me see if I can find you a Subaru.'"""
+
+user_car = input("What kind of rental car would you like to buy? ")
+print(f"The {user_car} will be available next week!")
