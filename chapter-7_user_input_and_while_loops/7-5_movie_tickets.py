e756d45935b556e9fb65f5cccce1d0cf7c77f928
Angie <i4ngela@gmail.com>
2025-01-12 19:22:52 -0300

    Exercise 7.5 solved.

---
+++ chapter-7_user_input_and_while_loops/7-5_movie_tickets.py
@@ -0,0 +1,27 @@
+"""A movie theater charges different ticket prices depending on a person's age.
+If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10;
+and if they are over age 12, the ticket is $15.
+Write a loop in which you ask users their age, and then tell them the cost of their movie ticket."""
+
+prompt = "\nThe ticket price depends on your age."
+prompt += "\nEnter your age or 'quit' to exit the program: "
+
+while True:
+    age = input(prompt)
+
+    if age.lower() == "quit":
+        break
+
+    age = int(age)
+
+    if age < 3:
+        ticket_price = 0
+    elif age <= 12:
+        ticket_price = 10
+    elif age > 12:
+        ticket_price = 15
+
+    if ticket_price == 0:
+        print("\nYour ticket is free!")
+    else:
+        print(f"\nYour ticket cost ${ticket_price}")
