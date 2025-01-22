a703c404c070c8d52978a4a0e883e209ecfeca0f
Angie <i4ngela@gmail.com>
2025-01-22 16:20:17 -0300

    fix: rename sandwishes_orders variable to sandwiches_orders in 8-12_sandwiches.py

--- chapter-8_functions/8-12_sandwiches.py
+++ chapter-8_functions/8-12_sandwiches.py
@@ -4,13 +4,13 @@ function call provides, and it should print a summary of the sandwich that's bei
 Call the function three times, using a different number of arguments each time."""
 
 
-def sandwishes_orders(*fillings):
+def sandwiches_orders(*fillings):
     """Print the list of fillings that have been requested."""
     print("Adding the following fillings to your sandwich:")
     for filling in fillings:
         print(f"- {filling.title()}")
 
 
-sandwishes_orders("tomato", "hamburger")
-sandwishes_orders("lettuce", "tomato", "cheese")
-sandwishes_orders("picles", "chicken breast")
+sandwiches_orders("tomato", "hamburger")
+sandwiches_orders("lettuce", "tomato", "cheese")
+sandwiches_orders("picles", "chicken breast")
