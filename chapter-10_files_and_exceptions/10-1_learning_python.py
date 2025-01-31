7d19fdb3d9d2d6dd9b75d8663a7cb596447bfde8
Angie <i4ngela@gmail.com>
2025-01-31 15:41:03 -0300

    Add Chapter 10, some txt files e solved 10.1 exercise.

---
+++ chapter-10_files_and_exceptions/10-1_learning_python.py
@@ -0,0 +1,21 @@
+"""Open a blank file in your text editor and write a few lines summarizing
+what you've learned about Python so far. Start each line with the phrase
+In Python you can. . . . Save the file as learning_python.txt in the same
+directory as your exercises from this chapter. Write a program that reads
+the file and prints what you wrote two times: print the contents once by
+reading in the entire file, and once by storing the lines in a list and
+then looping over each line."""
+
+from pathlib import Path
+
+path = Path("chapter-10_files_and_exceptions/files/learn_python.txt")
+contents = path.read_text()
+
+print(contents)
+
+lines = contents.splitlines()
+file_string = ""
+for line in lines:
+    file_string += line
+
+print(f"\n{file_string}")
