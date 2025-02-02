2577577e0b20a50405381e3480efd5888c3b8876
Angie <i4ngela@gmail.com>
2025-02-02 18:35:18 -0300

    Exercises 10.6 and 10.7 solved.

---
+++ chapter-10_files_and_exceptions/10-7_addition_calculator.py
@@ -0,0 +1,4 @@
+"""Wrap your code from Exercise 10-5 in a while loop so the user can continue
+entering numbers, even if they make a mistake and enter text instead of a number."""
+
+# Already done in exercise 10.6
